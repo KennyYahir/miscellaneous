01cc41c35090e7c362b397e3a2978d4025e14732
Kenny Yahir <kenny.puma1030@gmail.com>
2017-05-08 19:53:41 -0500

    Almost finished the Capa I y II

--- excavacion-ceramicas/R/exploratory_analysis.R
+++ excavacion-ceramicas/R/exploratory_analysis.R
@@ -30,6 +30,7 @@ MyBarBlot <- function(data, x.var, y.var,
   p1 <- p1 + coord_flip()
   p1 <- p1 + geom_text(fontface = 1, color = "black", size = 2.5, hjust = -0.05)
   
+  # Set the breaks
   p1 <- p1 + scale_y_continuous(breaks = function(lims) seq(0, lims[2], 
                                                             by = round(lims[2] / 6)),
                                 limits = c(0, 1.1*max(data[, y.var])))
@@ -44,6 +45,79 @@ MyBarBlot <- function(data, x.var, y.var,
   
   p1  <- p1 + xlab(x.title) + ylab(y.title)
   p1
+}
+
+MyScatterPlot1 <- function(data, x.var, y.var,
+                         x.title = x.var,
+                         y.title = y.var,
+                         fill.color){
+  
+  p1 <- ggplot(data = data, aes_string(x = x.var, y = y.var))
+  
+  p1 <- p1 + geom_point(color = fill.color)
+  
+  p1 <- p1 + theme(panel.grid.major = element_line(colour = "#C7C3A5",
+                                                   linetype = "dotted"),
+                   axis.ticks = element_blank(),
+                   panel.background = element_blank(),
+                   axis.title = element_text(size = 9),
+                   axis.text = element_text(size = 8),
+                   axis.text.x = element_text(angle = 90))
+  
+  p1  <- p1 + xlab(x.title) + ylab(y.title)
+  p1  
+}
+
+MyScatterPlot2 <- function(data, x.var, y.var,
+                           color.var,
+                           x.title = x.var,
+                           y.title = y.var){
+  
+  # Base
+  p1 <- ggplot(data = data, aes_string(x = x.var, y = y.var,
+                                       color = color.var))
+  
+  p1 <- p1 + geom_point()  # Points
+  
+  # Set the theme
+  p1 <- p1 + theme(panel.grid.major = element_line(colour = "#C7C3A5",
+                                                   linetype = "dotted"),
+                   axis.ticks = element_blank(),
+                   panel.background = element_blank(),
+                   axis.title = element_text(size = 9),
+                   axis.text = element_text(size = 8),
+                   axis.text.x = element_text(angle = 90))
+  
+  # Set x and y axis titles
+  p1  <- p1 + xlab(x.title) + ylab(y.title)
+  p1  # Final plot
+}
+
+
+MyScatterPlot3 <- function(data, x.var, y.var,
+                           fill.color = "orange",
+                           size.var,
+                           x.title = x.var,
+                           y.title = y.var){
+  
+  # Base
+  p1 <- ggplot(data = data, aes_string(x = x.var, y = y.var,
+                                       size = size.var))
+  
+  p1 <- p1 + geom_point(color = fill.color)  # Points
+  
+  # Set the theme
+  p1 <- p1 + theme(panel.grid.major = element_line(colour = "#C7C3A5",
+                                                   linetype = "dotted"),
+                   axis.ticks = element_blank(),
+                   panel.background = element_blank(),
+                   axis.title = element_text(size = 9),
+                   axis.text = element_text(size = 8),
+                   axis.text.x = element_text(angle = 90))
+  
+  # Set x and y axis titles
+  p1  <- p1 + xlab(x.title) + ylab(y.title)
+  p1  # Final plot
 }
 
 ###############################################################################
@@ -68,7 +142,7 @@ count.tipo2 <- mutate(count.tipo2, labs = paste0(comma(value),
                                                  ")"))
 
 
-# Make a Scatter plot
+# Make a Bar plot
 tipo.variedad <- MyBarBlot(count.tipo2, x.var = "Tipo.o.Variedad",
                 y.var = "value",
                 labs = "labs",
@@ -77,6 +151,19 @@ tipo.variedad <- MyBarBlot(count.tipo2, x.var = "Tipo.o.Variedad",
                 fill.color = "RoyalBlue")
 
 tipo.variedad + ggtitle("Capa I y II")
+
+
+# Make a Scatter plot of Temporalidad - Unidad
+count.tipo.variedad.unidad <- group_by(ddata, Tipo.o.Variedad, Unidad) %>%
+  summarise(Piezas = n())  # Count the pairs
+
+tipo.variedad.unidad <- MyScatterPlot3(count.tipo.variedad.unidad,
+                                      x.var = "Tipo.o.Variedad",
+                                      y.var = "Unidad",
+                                      fill.color = "orange",
+                                      size.var = "Piezas")
+tipo.variedad.unidad + ggtitle("Capa I y II")
+
 ###############################################################################
 
 # Counting by "Temporalidad"
@@ -99,7 +186,7 @@ count.tipo2 <- mutate(count.tipo2, labs = paste0(comma(value),
                                                  ")"))
 
 
-# Make a Scatter plot
+# Make a Bar plot
 temporalidad <- MyBarBlot(count.tipo2, x.var = "Temporalidad",
                 y.var = "value",
                 labs = "labs",
@@ -108,6 +195,20 @@ temporalidad <- MyBarBlot(count.tipo2, x.var = "Temporalidad",
                 fill.color = "RoyalBlue")
 temporalidad + ggtitle("Capa I y II")
 
+
+
+# Make a Scatter plot of Temporalidad - Unidad
+
+count.temporalidad.unidad <- group_by(ddata, Temporalidad, Unidad) %>%
+  summarise(Piezas = n())  # Count the pairs
+
+temporalidad.unidad <- MyScatterPlot3(count.temporalidad.unidad,
+                                           x.var = "Temporalidad",
+                                           y.var = "Unidad",
+                                           fill.color = "orange",
+                                           size.var = "Piezas"
+                                           )
+temporalidad.unidad + ggtitle("Capa I y II")
 
 ###############################################################################
 
